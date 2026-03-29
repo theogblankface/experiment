9d8ed4ff3f69779e19dcd413c60d3fc5ab173b68
Kyle Johnson <johnsonky56@gmail.com>
2026-03-29 21:54:59 +0000

    Added confirmation window to allow for audio/video content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+const consentPopup = document.getElementById('consent');
+const acceptBtn = document.getElementById('accept-btn');
+const declineBtn = document.getElementById('decline-btn');
+const xpAudio = document.querySelector('audio');
+
+declineBtn.addEventListener('click', () => {
+    // Try to go back in history, if not, go to Google
+    if (document.referrer) {
+        window.location.href = document.referrer;
+    } else {
+        window.location.href = 'https://www.google.com';
+    }
+});
+
+acceptBtn.addEventListener('click', () => {
+    consentPopup.style.display = 'none'; // Hide the overlay
+    // Modern browsers require a click to play audio, which is why autoplay didn't work outside of the live server 
+    // Clicking starts the XP sound
+    xpAudio.play(); 
+});
+
 const hoverLinks = document.querySelectorAll('.hoverLink');
 // Loops through each link
 hoverLinks.forEach((link) => {
